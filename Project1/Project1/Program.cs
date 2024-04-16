16e5e7594a0aa21f472b77774ebff1f3a84a4a7a
Wilson Njoroge Wanderi <wilsonnjoroge932@gmail.com>
2024-04-16 14:41:05 +0300

    Add project files.

---
+++ Project1/Project1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Wilson");
+
+            Console.ReadLine();
+        }
+    }
+}
