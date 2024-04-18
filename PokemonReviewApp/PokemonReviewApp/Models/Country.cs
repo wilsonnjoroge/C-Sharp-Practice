9cccacdbf15dc63f53247e1d389bd09be2747923
Wilson Njoroge Wanderi <wilsonnjoroge932@gmail.com>
2024-04-18 11:02:29 +0300

    addee the strings and variables

--- PokemonReviewApp/PokemonReviewApp/Models/Country.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace PokemonReviewApp.Models
-{
-    public class Country
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-    }
-}
--- PokemonReviewApp/PokemonReviewApp/Models/Review.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PokemonReviewApp.Models
-{
-    public class Review
-    {
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public string Text { get; set; }
-    }
-}
