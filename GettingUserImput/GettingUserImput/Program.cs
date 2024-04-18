a9387613a3317549a55beec2ebc0c9c7abf1180e
Wilson Njoroge Wanderi <wilsonnjoroge932@gmail.com>
2024-04-18 12:28:22 +0300

    Added a user imput for practice

---
+++ GettingUserImput/GettingUserImput/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettingUserImput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Dear " + name + ", Kindly enter your age: ");
+            string age = Console.ReadLine();
+
+            Console.Write("Dear " + name + ", You are " + age + " years old");
+
+            Console.ReadLine();
+        }
+    }
+}
