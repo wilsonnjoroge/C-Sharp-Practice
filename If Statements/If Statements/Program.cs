02c7153d19292bbf96e3f950e6693e76627d3864
Wilson Njoroge Wanderi <wilsonnjoroge932@gmail.com>
2024-04-19 09:37:12 +0300

    Added a more advanced calculator in C# for practice of the if statements

---
+++ If Statements/If Statements/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace If_Statements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isMale = true;
+
+            // If statement 
+            if (isMale)
+            {
+                Console.WriteLine("You can have a free Prostrate cancer check-up");
+            }
+
+
+            bool isHungry = false;
+
+            // If Else statement
+            if (isHungry)
+            {
+                Console.WriteLine("Enter the kitchen and cook");
+            }
+            else
+            {
+                Console.WriteLine("Go to the park and chill");
+            }
+
+            bool isFemale = true;
+            bool isTall = false;
+
+            // If, Else If, Else
+            if (isFemale && isTall)
+            {
+                Console.WriteLine("You are a tall female");
+            } else if (isFemale && !isTall)
+            {
+                Console.WriteLine("You are a female but not tall");
+            } else if ( !isFemale && isTall)
+            {
+                Console.WriteLine("You are not a female but you are tall");
+            } else
+            {
+                Console.WriteLine("You are neither a female nor tall");
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+}
