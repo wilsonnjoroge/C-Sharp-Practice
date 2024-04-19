248ebf3974e63852fe87d26739e7734ee988ffee
Wilson Njoroge Wanderi <wilsonnjoroge932@gmail.com>
2024-04-19 12:07:08 +0300

    Added a while and do while loops in C#

---
+++ While Loops/While Loops/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While_Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int index = 8;
+
+            // While Loop
+            while(index <= 5)
+            {
+                Console.WriteLine(index); // will not print any value 
+                index++;
+            }
+
+            // Do While Loop
+            do
+            {    
+                Console.WriteLine(index); // Will print value of index then try to increment it then the next code executed
+                index++;
+            }
+            while (index <= 5);
+
+
+            Console.ReadLine(); 
+        }
+    }
+}
