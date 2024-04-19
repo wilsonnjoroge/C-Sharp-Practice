2c1abff5ddbc2c1668831b2d0c17cef2d2173cb3
Wilson Njoroge Wanderi <wilsonnjoroge932@gmail.com>
2024-04-19 22:29:16 +0300

    Added a Exponent Method executing a for loop to get the power of a number.

---
+++ Exponent Method/Exponent Method/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exponent_Method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(getPower(3, 5));
+
+            Console.ReadLine();
+        }
+
+        static int getPower (int baseNum, int powerNum)
+        {
+            int result = 1;
+
+            for (int i = 0; i < powerNum; i++)
+            {
+                result = result * baseNum;
+            }
+
+
+            return result;
+        }
+    }
+}
