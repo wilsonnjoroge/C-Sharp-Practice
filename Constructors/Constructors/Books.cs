85485475e5d64424d4bfd92f3ea7823158d734ea
Wilson Njoroge Wanderi <wilsonnjoroge932@gmail.com>
2024-04-20 17:07:24 +0300

    created a book Constructor in C#

---
+++ Constructors/Constructors/Books.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    internal class Books
+    {
+        public string title;
+        public string author;
+        public int pages;
+
+        // Creating a constructor of the class (special method)
+        public Books (string aTitle, string aAuthor, int aPages)
+        {
+            title = aTitle;
+            author = aAuthor;
+            pages = aPages;
+        }
+    }
+}
