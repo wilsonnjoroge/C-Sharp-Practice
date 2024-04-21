4eb029ec3799ac2b4c2a5de902d2148e50182ed0
Wilson Njoroge Wanderi <wilsonnjoroge932@gmail.com>
2024-04-21 13:46:38 +0300

    pactice on Static Class Attributes

---
+++ Static Class Atributes/Static Class Atributes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Class_Atributes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Song winner = new Song("I am a winner", "Delemere", 300);
+            Console.WriteLine(Song.songCount);
+            Console.WriteLine(winner.title); // normal attributes of the Song class (Keeps on changing)
+            Song blessed = new Song("Blessed soul", "The Gospel Gurus", 600);
+            Console.WriteLine(Song.songCount);
+            Console.WriteLine(blessed.title); // normal attributes of the Song class (Keeps on changing)
+
+            // Static attribute
+            Console.WriteLine(Song.songCount);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Static Class Atributes/Static Class Atributes/Song.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Class_Atributes
+{
+    internal class Song
+    {
+        // attributes
+        public string title;
+        public string artist;
+        public int duration;
+        public static int songCount = 0;
+
+        // Constructor
+        public Song( string aTitle, string aArtist, int dDuration)
+        {
+            title = aTitle; // normal
+            artist = aArtist; // normal
+            duration = dDuration; // normal
+            songCount++; // Static
+        }
+    }
+}
